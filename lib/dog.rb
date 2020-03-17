919310c632090868beb483383ea1e0a9a6131340
Rwanda S <rwanda.ace@gmail.com>
2020-03-17 12:42:32 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Dog
+  def name=(doggo_name)
+    @this_doggos_name = doggo_name
+  end
+  def name
+    @this_doggos_name
+  end
+  def bark
+    puts "woof!"
+  end
+end
